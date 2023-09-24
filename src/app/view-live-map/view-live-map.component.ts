af5ae054d826f7de66dd6ab05ea153e10a9c5258
Kevin Kreulen <kreulenk@gmail.com>
2023-09-24 17:01:51 -0500

    Minor refactoring and zoom translation fixes

--- src/app/view-live-map/view-live-map.component.ts
+++ src/app/view-live-map/view-live-map.component.ts
@@ -35,13 +35,12 @@ type UserDataCollection = {
 export class ViewLiveMapComponent implements AfterViewInit {
   private allUsersData: UserDataCollection = {};
   private svg: any;
-  private xScale: any;
+  private originalXScale: any;
   private currentXScale: any;
   private xAxis: any;
-  private yScale: any;
+  private originalYScale: any;
   private currentYScale: any;
   private yAxis: any;
-  private svgPaths: any;
   private pageWidth: number = window.innerWidth * .99;
   private pageHeight: number = window.innerHeight * .99;
 
@@ -100,31 +99,33 @@ export class ViewLiveMapComponent implements AfterViewInit {
       .append("svg")
       .attr("width", this.pageWidth)
       .attr("height", this.pageHeight)
-      // .append("g")
 
     // Create the X-axis band scale
-    this.xScale = d3.scaleLinear()
+    this.originalXScale = d3.scaleLinear()
       .range([0, this.pageWidth])
       .domain([-3, 3]);
+    this.currentXScale = this.originalXScale;
 
     // Draw the X-axis on the DOM
     this.xAxis = this.svg.append("g")
       .attr("transform", "translate(" + 0 + "," + this.pageHeight / 2 + ")")
-      .call(d3.axisBottom(this.xScale));
+      .call(d3.axisBottom(this.originalXScale));
 
     // Create the Y-axis band scale
-    this.yScale = d3.scaleLinear()
+    this.originalYScale = d3.scaleLinear()
       .range([0, this.pageHeight])
       .domain([3, -3]);
+    this.currentYScale = this.originalYScale;
 
     // Draw the Y-axis on the DOM
     this.yAxis = this.svg.append("g")
       .attr("transform", "translate(" + this.pageWidth / 2 + "," + 0 + ")")
-      .call(d3.axisLeft(this.yScale));
+      .call(d3.axisLeft(this.originalYScale));
 
     var zoom: any = d3.zoom()
       .scaleExtent([0.5, 2])
       .extent([[0, 0], [this.pageWidth, this.pageHeight]])
+      .translateExtent([[0, 0], [this.pageWidth * 2, this.pageHeight * 2]] )
       .on('zoom', (e) => this.updatePerZoom(e));
 
     d3.select("div#bar")
@@ -133,8 +134,8 @@ export class ViewLiveMapComponent implements AfterViewInit {
 
   private updatePerZoom(event: any) {
     // Update the scales
-    this.currentXScale = event.transform.rescaleX(this.xScale);
-    this.currentYScale = event.transform.rescaleY(this.yScale);
+    this.currentXScale = event.transform.rescaleX(this.originalXScale);
+    this.currentYScale = event.transform.rescaleY(this.originalYScale);
     // Update the axes
     this.xAxis.call(d3.axisBottom(this.currentXScale))
     this.yAxis.call(d3.axisLeft(this.currentYScale))
@@ -143,15 +144,15 @@ export class ViewLiveMapComponent implements AfterViewInit {
   }
 
   private addNewUser(userData: UserData): void {
-    this.svgPaths = this.svg.append("path")
+    this.svg.append("path")
       .datum(userData.dataToPlot)
       .attr("fill", "none")
       .attr("stroke", userData.lineColor)
       .attr("id", userData.username)
       .attr("stroke-width", 2.5)
       .attr("d", d3.line()
-        .x((d: any) => this.xScale(d.adjustedLatitude))
-        .y((d: any) => this.yScale(d.adjustedLongitude))
+        .x((d: any) => this.currentXScale(d.adjustedLatitude))
+        .y((d: any) => this.currentYScale(d.adjustedLongitude))
       );
   }
 
