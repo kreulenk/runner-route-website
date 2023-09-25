2f85988ecbb9eb2d6389e2c0949b679652adf07c
Kevin Kreulen <kreulenk@gmail.com>
2023-09-25 18:28:50 -0500

    Fixed tooltip logic to make it visible

--- src/app/live-map/live-map.component.ts
+++ src/app/live-map/live-map.component.ts
@@ -22,7 +22,6 @@ export class LiveMapComponent implements AfterViewInit {
   private pageWidth: number = window.innerWidth * .99;
   private pageHeight: number = window.innerHeight * .99;
 
-
   private createSvg(): void {
     this.svg = d3.select("div#bar")
       .append("svg")
@@ -32,7 +31,7 @@ export class LiveMapComponent implements AfterViewInit {
     // Create the X-axis band scale
     this.originalXScale = d3.scaleLinear()
       .range([0, this.pageWidth])
-      .domain([-3, 3]);
+      .domain([-2, 2]);
     this.currentXScale = this.originalXScale;
 
     // Draw the X-axis on the DOM
@@ -43,7 +42,7 @@ export class LiveMapComponent implements AfterViewInit {
     // Create the Y-axis band scale
     this.originalYScale = d3.scaleLinear()
       .range([0, this.pageHeight])
-      .domain([3, -3]);
+      .domain([2, -2]);
     this.currentYScale = this.originalYScale;
 
     // Draw the Y-axis on the DOM
@@ -51,10 +50,10 @@ export class LiveMapComponent implements AfterViewInit {
       .attr("transform", "translate(" + this.pageWidth / 2 + "," + 0 + ")")
       .call(d3.axisLeft(this.originalYScale));
 
-    var zoom: any = d3.zoom()
+    let zoom: any = d3.zoom()
       .scaleExtent([0.5, 2])
       .extent([[0, 0], [this.pageWidth, this.pageHeight]])
-      .translateExtent([[-this.pageWidth, -this.pageHeight], [this.pageWidth * 2, this.pageHeight * 2]] )
+      .translateExtent([[-this.pageWidth, -this.pageHeight], [this.pageWidth * 2, this.pageHeight * 2]])
       .on('zoom', (e) => this.updatePerZoom(e));
 
     d3.select("div#bar")
@@ -73,21 +72,62 @@ export class LiveMapComponent implements AfterViewInit {
   }
 
   public addNewUser(userData: UserData): void {
+    // Add the path element that will track the user
     this.svg.append("path")
       .datum(userData.dataToPlot)
+      .attr("id", userData.username)
       .attr("fill", "none")
       .attr("stroke", userData.lineColor)
-      .attr("id", userData.username)
       .attr("stroke-width", 2.5)
       .attr("d", d3.line()
         .x((d: any) => this.currentXScale(d.adjustedLatitude))
         .y((d: any) => this.currentYScale(d.adjustedLongitude))
       );
+
+    let Tooltip = d3.select("div#bar")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+      .style("position", "fixed")
+
+    let mouseover: any = (event: any) => {
+      d3.select("path#" + userData.username)
+        .attr("stroke-width", 6)
+
+      Tooltip
+        .style("opacity", 1)
+        .html("test")
+        .style("left", event.x + 5 + "px")
+        .style("top", event.y + 5 + "px")
+    }
+    let mousemove:any = (event: any) => {
+      Tooltip
+        .html("test")
+        .style("left", event.x + 5 + "px")
+        .style("top", event.y + 5 +"px")
+    }
+    let mouseout: any = () => {
+      d3.select("path#" + userData.username)
+        .attr("stroke-width", 2.5)
+
+      Tooltip
+        .style("opacity", 0)
+    }
+    
+    d3.select("path#" + userData.username)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseout", mouseout)
   }
 
   public plotUserLines() {
     for (const [key, value] of Object.entries(this.allUsersData)) {
-      this.svg.select("#" + value.username)
+      this.svg.select("path#" + value.username)
         .datum(value.dataToPlot)
         .attr("d", d3.line()
           .x((d: any) => this.currentXScale(d.adjustedLatitude))
